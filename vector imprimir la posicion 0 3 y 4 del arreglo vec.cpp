38e5c18bf1ae5e0d618698b2d81d5283742726d6
Anabel Berumen <berumen.anabel@gmail.com>
2017-07-08 05:44:44 +0200

    vector imprimir la posicion

---
+++ vector imprimir la posicion 0 3 y 4 del arreglo vec.cpp
@@ -0,0 +1,14 @@
+// vector imprimir la posicion 0 3 y 4 del arreglo vec
+#include <stdio.h>
+#include <conio.h>
+#include "stdlib.h"
+
+main(){
+	int vec [5]= {3,5,7,9,2};
+	system ("clr");
+	printf("\n posición 0 %d",vec[0]);
+	printf("\n posición 3 %d",vec[3]);
+	printf("\n posición 4 %d",vec[4]);
+	getch();
+	return 0;
+}
