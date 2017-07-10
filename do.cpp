1848e4764963b5ca7c7042c0c6380fa0cdae4d9f
Anabel Berumen <berumen.anabel@gmail.com>
2017-07-10 03:27:17 +0200

    do while

---
+++ do.cpp
@@ -0,0 +1,12 @@
+//do-while
+#include <stdio.h>
+#include <conio.h>
+main (){
+	int i=1;
+	do{
+		printf("%i\n",i);
+		i++;
+	}while (i<10);
+	printf ("oprima una tecla para continuar");
+	getch ();
+}
