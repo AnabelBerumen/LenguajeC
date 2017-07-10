81f6c977b72dba9c2e1e4f77026cb4259d5053de
Anabel Berumen <berumen.anabel@gmail.com>
2017-07-10 03:18:59 +0200

    while

---
+++ while.cpp
@@ -0,0 +1,13 @@
+// while
+#include <stdio.h>
+#include <conio.h>
+
+main (){
+	int i=1;
+	while (i<=10){
+		printf ("%i \n",i);
+		i++;
+	}
+	printf ("oprima una tecla para continuar");
+	getch();
+}
